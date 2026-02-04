05fd19c98af2faa646c20e8d7bf0f3864f4c9b79
horn553 <36446753+horn553@users.noreply.github.com>
2026-02-04 21:51:41 +0900

    🎉 検証環境の基本を作成

---
+++ src/lib/Fab.svelte
@@ -0,0 +1,24 @@
+<div><span>+</span></div>
+
+<style>
+	div {
+		position: fixed;
+		bottom: 0;
+		right: 0;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		border-radius: 25px;
+		width: 50px;
+		height: 50px;
+
+		background-color: white;
+	}
+
+	span {
+		font-size: 30px;
+		font-weight: bold;
+	}
+</style>
---
+++ src/lib/Gradient.svelte
@@ -0,0 +1,13 @@
+<div class="gradient"></div>
+
+<style>
+	div {
+		width: 200vw;
+		height: 200vh;
+	}
+
+	.gradient {
+		background: #3f5efb;
+		background: radial-gradient(circle, rgba(63, 94, 251, 1) 0%, rgba(252, 70, 107, 1) 100%);
+	}
+</style>
